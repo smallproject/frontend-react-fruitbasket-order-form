4bd31f0968bb944b3ff58a69ac6058cf6cb9d334
smallproject <j.a.villas@live.com.ph>
2024-09-12 18:39:23 +0200

    improves functionality - works on component level

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,54 @@
 
 import './App.css'
 import TileFruit from "./components/TileFruit.jsx";
+import React from "react";
+
 
 function App() {
+
+    // const [count, setCount] = React.useState(0);
+    //
+    // const handleIncrement = () => {
+    //     setCount(count + 1);
+    // };
 
   return (
     <>
         <h1>Fruitmand bezorgservice</h1>
 
+        <div>
         <TileFruit
             imageUrl={"./assets/images/logo.png"}
             imageAlt={"Aarbeien image"}
             fruitName={"Aarbeien"}
         />
+
+            {/*<p>{count}</p>*/}
+            {/*<button onClick={handleIncrement}></button>*/}
+        {/*<button*/}
+        {/*    onClick={() => setCount((count) => count + 1)}*/}
+        {/*    className={"addition"}>*/}
+        {/*    +*/}
+        {/*</button>*/}
+        </div>
+
+        <TileFruit
+            imageUrl={"./assets/images/logo.png"}
+            imageAlt={"Bananen image"}
+            fruitName={"Bananen"}
+        />
+        <TileFruit
+            imageUrl={"./assets/images/logo.png"}
+            imageAlt={"Appels image"}
+            fruitName={"Appels"}
+        />
+        <TileFruit
+            imageUrl={"./assets/images/logo.png"}
+            imageAlt={"Kiwi's image"}
+            fruitName={"Kiwi's"}
+        />
+
+        <button>RESET</button>
     </>
   )
 }
--- src/components/TileFruit.jsx
+++ src/components/TileFruit.jsx
@@ -1,15 +1,33 @@
 import "./TileFruit.css";
+import React from "react";
+
+// const [count, setCount] = React.useState(0);
+
+// function handleOnClick() {
+//     setCount(count + 1);
+// }
 
 function TileFruit({imageUrl, imageAlt,fruitName}) {
+    const [count, setCount] = React.useState(0);
+
+    const handleIncrement = () => {
+        setCount(count + 1);
+    };
+
+    const handleDecrement = () => {
+        if (count !== 0)
+            setCount(count - 1);
+    };
+
     return (
         <>
             <div className={"wrapper"}>
                 <div className={"tile-fruit"}>
                     <img src={imageUrl} alt={imageAlt}/>
                     <span>{fruitName}</span>
-                    <button className={"minus"}>-</button>
-                    <input className={"input-counter"} type="text" value={"0"}/>
-                    <button className={"addition"}>+</button>
+                    <button onClick={handleDecrement} className={"minus"}>-</button>
+                    <input className={"input-counter"} type="text" value={count}/>
+                    <button onClick={handleIncrement} className={"addition"}>+</button>
                 </div>
             </div>
         </>
